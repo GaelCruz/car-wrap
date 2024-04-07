3b0223920d3c9850662b055d78bd624245b3a9c9
GaelCruz <108242626+GaelCruz@users.noreply.github.com>
2024-04-07 00:16:26 -0500

    added google-reviews

--- index.js
+++ index.js
@@ -4,15 +4,5 @@ const serviceInfo = document.querySelector('.card-info');
 const rightArrow = document.querySelector('#right-arrow');
 const leftArrow = document.querySelector('#left-arrow');
 
-// function serviceBlurHoverOn(){
-//   service.style.filter = 'blur(5px)';
-// };
 
-// function serviceBlurHoverOff(){
-//   service.style.filter = 'blur(0px)';
-
-// }
-
-// service.addEventListener('mouseover', serviceBlurHoverOn)
-// service.addEventListener('mouseleave', serviceBlurHoverOff)
 
