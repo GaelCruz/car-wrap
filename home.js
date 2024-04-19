aa407f2defcd81d16114c758a37d882210c8b7f4
GaelCruz <gaelecruz18@gmail.com>
2024-04-19 12:33:10 -0500

    added home.js file to work on services carousel

---
+++ home.js
@@ -0,0 +1,4 @@
+const serviceContainer = document.getElementById("service-container")
+const rightArrow = document.querySelector("right-arrow")
+const leftArrow = document.querySelector("left-arrow")
+
